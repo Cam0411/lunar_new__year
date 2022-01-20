ee7c01cb2459564e438e4e467d5375b3ade1e039
Cam0411 <89090731+Cam0411@users.noreply.github.com>
2022-01-20 12:36:33 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const newYear = new Date(2022,1,1,0,0,0,0).getTime()
+const dayspan = document.querySelector(".daySpan")
+const hourSpan = document.querySelector(".hoursSpan")
+const minSpan = document.querySelector(".minSpan")
+const secSpan = document.querySelector(".secSpan")
+
+function renderTime(){
+   const today = new Date().getTime()
+   const timeRemain = newYear - today
+
+   const sec = 1000 
+   const min = sec * 60 
+   const hour = min * 60
+   const day = hour * 24
+
+   dayspan.innerHTML = Math.round(timeRemain / (day)) 
+   hourSpan.innerHTML = Math.round( (timeRemain % day) / (hour))
+   minSpan.innerHTML = Math.round( (timeRemain % hour) / (min) )
+   secSpan.innerHTML =Math.round( (timeRemain % min) / (sec)  )
+}
+
+setInterval(renderTime,1000)
